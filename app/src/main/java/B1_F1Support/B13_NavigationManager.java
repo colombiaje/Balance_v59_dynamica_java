ef28c1e7b9d6f8303215b29bfcb381ab8d34421b
colombiaje <59103634+colombiaje@users.noreply.github.com>
2026-09-05 10:55:11 -0500

    Update B13_NavigationManager.java

--- app/src/main/java/B1_F1Support/B13_NavigationManager.java
+++ app/src/main/java/B1_F1Support/B13_NavigationManager.java
@@ -493,6 +493,6 @@ public class B13_NavigationManager {
         }
 
     }
-    //comentario prueba git XXX1
+    //comentario prueba git XXX12 Remoto
     // Version REMOTA del comentario de prueba
 }
